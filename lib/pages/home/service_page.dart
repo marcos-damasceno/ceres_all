a4f587f87be3ac12c8cf5b84919edc74cd0fa4d0
Marcos Damasceno <marcos.damasceno@ceresinvestimentos.com>
2023-05-26 17:05:16 -0300

    feat: layout da tela de serviços do aplicativo

--- lib/pages/home/service_page.dart
+++ lib/pages/home/service_page.dart
@@ -7,23 +7,143 @@ class ServicePage extends StatefulWidget {
   State<ServicePage> createState() => _ServicePageState();
 }
 
-class _ServicePageState extends State<ServicePage> {
+class _ServicePageState extends State<ServicePage>
+    with SingleTickerProviderStateMixin {
+  TabController? tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: 2, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.redAccent,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Text(
-              'Tela de módulos disponiveis',
-              style: TextStyle(
-                fontSize: 40,
-                color: Colors.black87,
+      bottomNavigationBar: Material(
+        color: const Color(0xff274059),
+        shadowColor: Colors.black87.withOpacity(0.5),
+        child: TabBar(
+          controller: tabController,
+          indicatorColor: Colors.white,
+          tabs: const [
+            Tab(
+              icon: Icon(
+                Icons.home,
+                color: Colors.white,
               ),
             ),
+            Tab(
+              icon: Icon(
+                Icons.chat_bubble,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'Profile',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        elevation: 0,
+        backgroundColor: const Color(0xff274059),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            color: Colors.white,
+          ),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.more_vert,
+              color: Colors.white,
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Container(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(top: 10),
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 2 - 40,
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black87.withOpacity(0.7),
+                      blurRadius: 20,
+                      spreadRadius: 10)
+                ],
+                color: const Color(0xff274059),
+                borderRadius: const BorderRadius.only(
+                  bottomRight: Radius.circular(30),
+                  bottomLeft: Radius.circular(30),
+                ),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 130),
+                        child: Container(
+                          height: 105,
+                          width: 105,
+                          decoration: BoxDecoration(
+                              color: const Color(0xff274059),
+                              borderRadius: BorderRadius.circular(52.5),
+                              boxShadow: const [
+                                BoxShadow(
+                                  color: Colors.redAccent,
+                                  spreadRadius: 2,
+                                ),
+                              ]),
+                          child: const CircleAvatar(
+                            radius: 50,
+                            backgroundImage:
+                                AssetImage('assets/photos/selena.jpg'),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  const Text(
+                    '@marcos.damasceno',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: 35),
+                    child: Text(
+                      'Marcos Damasceno',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
