eebbbebab060691354915cfd7de4e3231a7169f8
marcos.damasceno <marcosviniciussd@hotmail.com>
2023-06-11 21:33:20 -0300

    feat: criação dos card do aplicativo

--- lib/pages/home/service_page.dart
+++ lib/pages/home/service_page.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class ServicePage extends StatefulWidget {
@@ -15,6 +16,62 @@ class _ServicePageState extends State<ServicePage>
   void initState() {
     super.initState();
     tabController = TabController(length: 2, vsync: this);
+  }
+
+  final _listCard = [
+    {
+      'title': 'Chamados',
+      'image': 'assests/icons/icon1.png',
+      'description': 'Abertura e acompanhamento de chamados',
+    },
+    {
+      'title': 'Reunião',
+      'image': 'assests/icons/icon1.png',
+      'description': 'Abertura e acompanhamento de chamados',
+    },
+    {
+      'title': 'Cantina',
+      'image': 'assests/icons/icon1.png',
+      'description': 'Abertura e acompanhamento de chamados',
+    },
+    {
+      'title': 'Tarefas',
+      'image': 'assests/icons/icon1.png',
+      'description': 'Abertura e acompanhamento de chamados',
+    }
+  ];
+
+  final List<String> imgList = [
+    'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+    'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+    'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+    'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+    'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+    'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+  ];
+
+  int _currentIndex = 0;
+  List cardList = [
+    const Item1(
+      key: Key('Chamados'),
+    ),
+    const Item2(
+      key: Key('Reserva'),
+    ),
+    const Item3(
+      key: Key('Cantina'),
+    ),
+    const Item4(
+      key: Key('Solicitações'),
+    )
+  ];
+
+  List<T> map<T>(List list, Function handler) {
+    List<T> result = [];
+    for (var i = 0; i < list.length; i++) {
+      result.add(handler(i, list[i]));
+    }
+    return result;
   }
 
   @override
@@ -287,9 +344,310 @@ class _ServicePageState extends State<ServicePage>
                   ),
                 ],
               ),
-            )
+            ),
+            const SizedBox(height: 40),
+            /*
+            const Text(
+              'Texto Aqui',
+              style: TextStyle(color: Colors.black87, fontSize: 30),
+            ),
+            */
+            Column(
+              children: [
+                CarouselSlider(
+                  items: cardList.map((card) {
+                    return Builder(builder: (BuildContext context) {
+                      return SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.30,
+                        width: MediaQuery.of(context).size.width,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Card(
+                            elevation: 3,
+                            color: Colors.white,
+                            //color: const Color(0xff274059),
+                            child: card,
+                          ),
+                        ),
+                      );
+                    });
+                  }).toList(),
+                  options: CarouselOptions(
+                    height: 300.0,
+                    autoPlay: false,
+                    autoPlayInterval: const Duration(seconds: 10),
+                    autoPlayAnimationDuration: const Duration(
+                      milliseconds: 10000,
+                    ),
+                    autoPlayCurve: Curves.fastOutSlowIn,
+                    pauseAutoPlayOnTouch: true,
+                    aspectRatio: 2.0,
+                    onPageChanged: (index, reason) {
+                      setState(() {
+                        _currentIndex = index;
+                      });
+                    },
+                  ),
+                ),
+                /*
+                const Row(
+                  children: [
+                    Text(
+                      'Ou você vira lobo ou aceita que faz parte do rebanho!',
+                      style: TextStyle(
+                        color: Colors.black87,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                )
+                */
+              ],
+            ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+/*
+class Item1 extends StatelessWidget {
+  const Item1({required Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            stops: [0.3, 1],
+            colors: [Color(0xffff4000),Color(0xffffcc66),]
+        ),
+      ),
+      child: const Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+              "Chamados",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold
+              )
+          ),
+          Text(
+              "Chamados",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 17.0,
+                  fontWeight: FontWeight.w600
+              )
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Item2 extends StatelessWidget {
+  const Item2({required Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            stops: [0.3, 1],
+            colors: [Color(0xff5f2c82), Color(0xff49a09d)]
+        ),
+      ),
+      child: const Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+              "Reserva de Sala",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold
+              )
+          ),
+          Text(
+              "Reserva de Sala",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 17.0,
+                  fontWeight: FontWeight.w600
+              )
+          ),
+        ],
+      ),
+    );
+  }
+}
+*/
+
+class Item1 extends StatelessWidget {
+  const Item1({required Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            stops: [
+              0.3,
+              1,
+            ],
+            colors: [
+              Color(0xff274059),
+              Color(0xff274059),
+            ]),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Text("Chamados",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold)),
+          const SizedBox(
+            height: 20,
+          ),
+          Image.asset(
+            'assets/icons/atendimento.png',
+            height: 180.0,
+            fit: BoxFit.fill,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Item2 extends StatelessWidget {
+  const Item2({required Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            stops: [
+              0.3,
+              1,
+            ],
+            colors: [
+              Color(0xff274059),
+              Color(0xff274059),
+            ]),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Text("Reserva de Salas",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold)),
+          const SizedBox(
+            height: 20,
+          ),
+          Image.asset(
+            'assets/icons/reserva.png',
+            height: 180.0,
+            fit: BoxFit.cover,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Item3 extends StatelessWidget {
+  const Item3({required Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            stops: [
+              0.3,
+              1
+            ],
+            colors: [
+              Color(0xff274059),
+              Color(0xff274059),
+            ]),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Text("Cantina",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold)),
+          const SizedBox(
+            height: 20,
+          ),
+          Image.asset(
+            'assets/icons/cantina.png',
+            height: 180.0,
+            fit: BoxFit.cover,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Item4 extends StatelessWidget {
+  const Item4({required Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            stops: [
+              0.3,
+              1
+            ],
+            colors: [
+              Color(0xff274059),
+              Color(0xff274059),
+            ]),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Text("Solicitações",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold)),
+          const SizedBox(
+            height: 20,
+          ),
+          Image.asset(
+            'assets/icons/smartphone.png',
+            height: 180.0,
+            fit: BoxFit.cover,
+          )
+        ],
       ),
     );
   }
