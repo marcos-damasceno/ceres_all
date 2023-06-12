45d37f56ea44e3b89a03606d11f12d46f4b819d5
Marcos Damasceno <marcos.damasceno@ceresinvestimentos.com>
2023-06-12 12:56:39 -0300

    feat: ajustes na classe de card

--- lib/pages/home/service_page.dart
+++ lib/pages/home/service_page.dart
@@ -1,4 +1,5 @@
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:ceres_all/pages/home/service_card.dart';
 import 'package:flutter/material.dart';
 
 class ServicePage extends StatefulWidget {
@@ -18,6 +19,7 @@ class _ServicePageState extends State<ServicePage>
     tabController = TabController(length: 2, vsync: this);
   }
 
+  /*
   final _listCard = [
     {
       'title': 'Chamados',
@@ -40,15 +42,7 @@ class _ServicePageState extends State<ServicePage>
       'description': 'Abertura e acompanhamento de chamados',
     }
   ];
-
-  final List<String> imgList = [
-    'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
-    'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
-    'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
-    'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
-    'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
-    'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
-  ];
+  */
 
   int _currentIndex = 0;
   List cardList = [
@@ -63,7 +57,8 @@ class _ServicePageState extends State<ServicePage>
     ),
     const Item4(
       key: Key('Solicitações'),
-    )
+    ),
+    const ServiceCard(key: Key('Teste')),
   ];
 
   List<T> map<T>(List list, Function handler) {
@@ -138,9 +133,10 @@ class _ServicePageState extends State<ServicePage>
               decoration: BoxDecoration(
                 boxShadow: [
                   BoxShadow(
-                      color: Colors.black87.withOpacity(0.7),
-                      blurRadius: 20,
-                      spreadRadius: 10)
+                    color: Colors.black87.withOpacity(0.7),
+                    blurRadius: 20,
+                    spreadRadius: 10,
+                  ),
                 ],
                 color: const Color(0xff274059),
                 borderRadius: const BorderRadius.only(
@@ -346,12 +342,6 @@ class _ServicePageState extends State<ServicePage>
               ),
             ),
             const SizedBox(height: 40),
-            /*
-            const Text(
-              'Texto Aqui',
-              style: TextStyle(color: Colors.black87, fontSize: 30),
-            ),
-            */
             Column(
               children: [
                 CarouselSlider(
@@ -386,19 +376,6 @@ class _ServicePageState extends State<ServicePage>
                     },
                   ),
                 ),
-                /*
-                const Row(
-                  children: [
-                    Text(
-                      'Ou você vira lobo ou aceita que faz parte do rebanho!',
-                      style: TextStyle(
-                        color: Colors.black87,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ],
-                )
-                */
               ],
             ),
           ],
@@ -408,84 +385,6 @@ class _ServicePageState extends State<ServicePage>
   }
 }
 
-/*
-class Item1 extends StatelessWidget {
-  const Item1({required Key key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            stops: [0.3, 1],
-            colors: [Color(0xffff4000),Color(0xffffcc66),]
-        ),
-      ),
-      child: const Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-              "Chamados",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 22.0,
-                  fontWeight: FontWeight.bold
-              )
-          ),
-          Text(
-              "Chamados",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 17.0,
-                  fontWeight: FontWeight.w600
-              )
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class Item2 extends StatelessWidget {
-  const Item2({required Key key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            stops: [0.3, 1],
-            colors: [Color(0xff5f2c82), Color(0xff49a09d)]
-        ),
-      ),
-      child: const Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-              "Reserva de Sala",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 22.0,
-                  fontWeight: FontWeight.bold
-              )
-          ),
-          Text(
-              "Reserva de Sala",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 17.0,
-                  fontWeight: FontWeight.w600
-              )
-          ),
-        ],
-      ),
-    );
-  }
-}
-*/
-
 class Item1 extends StatelessWidget {
   const Item1({required Key key}) : super(key: key);
 
@@ -493,14 +392,6 @@ class Item1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: const BoxDecoration(
-        /*
-        boxShadow: [
-          BoxShadow(
-              color: Colors.black87.withOpacity(0.5),
-              blurRadius: 8,
-              spreadRadius: 4)
-        ],
-        */
         gradient: LinearGradient(
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
@@ -657,3 +548,81 @@ class Item4 extends StatelessWidget {
     );
   }
 }
+
+/*
+class Item1 extends StatelessWidget {
+  const Item1({required Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            stops: [0.3, 1],
+            colors: [Color(0xffff4000),Color(0xffffcc66),]
+        ),
+      ),
+      child: const Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+              "Chamados",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold
+              )
+          ),
+          Text(
+              "Chamados",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 17.0,
+                  fontWeight: FontWeight.w600
+              )
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Item2 extends StatelessWidget {
+  const Item2({required Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            stops: [0.3, 1],
+            colors: [Color(0xff5f2c82), Color(0xff49a09d)]
+        ),
+      ),
+      child: const Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+              "Reserva de Sala",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold
+              )
+          ),
+          Text(
+              "Reserva de Sala",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 17.0,
+                  fontWeight: FontWeight.w600
+              )
+          ),
+        ],
+      ),
+    );
+  }
+}
+*/
